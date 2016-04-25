86aacd9ab3ccf9b4d9dcfe515df94f4010704ea6
Juan Pablo Manzano <juanpi9_@hotmail.com>
2016-04-25 08:34:26 -0500

    Actualización de repositorio

---
+++ app/src/main/java/juanpablomanzano/myapp/MainActivity.java
@@ -0,0 +1,16 @@
+package juanpablomanzano.myapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        int alma = 34;
+        /*este es un comentario*/
+
+    }
+}
